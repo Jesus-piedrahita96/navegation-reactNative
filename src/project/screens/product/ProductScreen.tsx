0e6fe765da86b17d6f529b08fbe98a8d35e6c2f3
jesusPiedrahita <programador9606@gmail.com>
2024-03-08 16:35:25 -0500

    manejando goBack(), StackAction.popToTop()

--- src/project/screens/product/ProductScreen.tsx
+++ src/project/screens/product/ProductScreen.tsx
@@ -1,15 +1,37 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-import {RouteProp, useRoute} from '@react-navigation/native';
+import {
+  RouteProp,
+  StackActions,
+  useNavigation,
+  useRoute,
+} from '@react-navigation/native';
 import {RootStackParams} from '../../routes/StackNavigation';
+import {globalStyles} from '../../themes/theme';
+import {Button} from 'react-native-paper';
 
 export default function ProductScreen() {
   const params = useRoute<RouteProp<RootStackParams, 'Product'>>().params;
+  const navigation = useNavigation();
 
   return (
     <View>
-      <Text>ProductScreen</Text>
-      <Text>{params.name}</Text>
+      <Text style={globalStyles.title}>ProductScreen</Text>
+      <Text style={globalStyles.title}>{params.name}</Text>
+      {/* retrodeceder de acuerdo al historial */}
+      <Button
+        mode="contained-tonal"
+        onPress={() => navigation.goBack()}
+        style={globalStyles.styleButton}>
+        Regresar
+      </Button>
+      {/* regresar al inicio establecido por la navegacion stack */}
+      <Button
+        mode="contained-tonal"
+        onPress={() => navigation.dispatch(StackActions.popToTop())}
+        style={globalStyles.styleButton}>
+        ir al Home{' '}
+      </Button>
     </View>
   );
 }
